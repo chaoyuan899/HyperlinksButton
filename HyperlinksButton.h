58eb17587520541df05866213a9d290b9a2bc512
chaoyuan899 <chaoyuan899@gmail.com>
2014-07-30 20:43:06 +0800

    add HyperlinksButton.h and .m files

---
+++ HyperlinksButton.h
@@ -0,0 +1,16 @@
+//
+//  HyperlinksButton.h
+//  memberapp
+//
+//  Created by sam on 4/30/14.
+//  Copyright (c) 2014 com.by-health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HyperlinksButton : UIButton
+{
+    UIColor *lineColor;
+}
+-(void)setColor:(UIColor*)color;
+@end
---
+++ HyperlinksButton.m
@@ -0,0 +1,43 @@
+//
+//  HyperlinksButton.m
+//  memberapp
+//
+//  Created by sam on 4/30/14.
+//  Copyright (c) 2014 com.by-health. All rights reserved.
+//
+
+#import "HyperlinksButton.h"
+
+@implementation HyperlinksButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(void)setColor:(UIColor *)color{
+    lineColor = [color copy];
+    [self setNeedsDisplay];
+}
+
+
+- (void) drawRect:(CGRect)rect {
+    CGRect textRect = self.titleLabel.frame;
+    CGContextRef contextRef = UIGraphicsGetCurrentContext();
+    
+    CGFloat descender = self.titleLabel.font.descender;
+    if([lineColor isKindOfClass:[UIColor class]]){
+        CGContextSetStrokeColorWithColor(contextRef, lineColor.CGColor);
+    }
+    
+    CGContextMoveToPoint(contextRef, textRect.origin.x, textRect.origin.y + textRect.size.height + descender+1);
+    CGContextAddLineToPoint(contextRef, textRect.origin.x + textRect.size.width, textRect.origin.y + textRect.size.height + descender+1);
+    
+    CGContextClosePath(contextRef);
+    CGContextDrawPath(contextRef, kCGPathStroke);
+}
+@end
